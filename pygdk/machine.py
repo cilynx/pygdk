dc80bd7a74d62d29ad2fddabfe7be7c64af51b1c
Randy C. Will <randall.will@gmail.com>
2021-08-29 15:42:16 -0700

    Machine profile (JSON) scaffolding

--- pygdk/machine.py
+++ pygdk/machine.py
@@ -1,3 +1,7 @@
+import json
+import os
+import sys
+
 MACHINE = '\033[91m'
 ENDC = '\033[0m'
 
@@ -6,16 +10,47 @@ class Machine:
     MILL = 'Mill'
     LATHE = 'Lathe'
 
-    def __init__(self, name=None, type=MILL, feed=500):
+    def __init__(self, name=None, type=None, max_feed=None):
+        if os.path.exists(name):
+            if type is not None or max_feed is not None:
+                raise ValueError(f"Machine must be initialized by JSON or parameters, but not both")
+            print(f";{MACHINE} Loading Machine parameters from JSON{ENDC}")
+            f = open(name)
+            dict = json.load(f)
+            f.close()
+            name = dict['Name']
+            type = dict['Type']
+            max_feed = dict['Max Feed Rate (mm/min)']
+            self._max_rpm = dict['Max Spindle RPM']
         if not type in [self.MILL, self.LATHE]:
             raise ValueError(f"Machine type ({type}) must be Machine.MILL or Machine.LATHE")
         self._type = type
-        self._feed = feed
+        self._max_feed = max_feed
         if name is None:
             self._name = type
         else:
             self._name = name
         print(f";{MACHINE} Initializing a {self.type} named {self.name}{ENDC}")
+
+    @property
+    def json(self):
+        dict = {
+            "Name": self._name,
+            "Type": self._type,
+            "Max Feed Rate (mm/min)": self._max_feed,
+            "Max Spindle RPM": self._max_rpm
+        }
+        return json.dumps(dict, indent=2)
+
+    def save_json(self, file, prompt=True):
+        if prompt and os.path.exists(file):
+            print(f"Overwrite {file}? (y/n) ")
+            response = sys.stdin.read(1)
+            if response != 'y':
+                return
+        f = open(file, 'w')
+        f.write(self.json + '\n')
+        f.close()
 
     def new_tool(self):
         from .tool import Tool
