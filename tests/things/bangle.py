40ca025ecd8c172aa43511e0ac5b4c476199df1e
Randy C. Will <randall.will@gmail.com>
2021-10-11 19:56:32 -0700

    Update bangle for current interface

--- tests/things/bangle.py
+++ tests/things/bangle.py
@@ -4,9 +4,9 @@ from pygdk import Machine
 
 onefinity = Machine('onefinity.json')
 
-onefinity.current_tool = '1/4" Downcut'
+onefinity.material = 'Soft Wood'
 
-onefinity.feed = 1000
+onefinity.tool = '1/4" Downcut'
 
 onefinity.helix(c_x=0, c_y=0, diameter=67, depth=21, z_step=10)
 
