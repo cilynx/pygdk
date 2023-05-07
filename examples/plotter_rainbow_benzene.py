dd27fc39a9defa5aa791626018be1334ae0f89cf
Randy Will <randall.will@gmail.com>
2023-05-06 20:23:18 -0600

    Tuneup

--- examples/plotter_rainbow_benzene.py
+++ examples/plotter_rainbow_benzene.py
@@ -4,16 +4,20 @@ from pygdk import Plotter
 onefinity = Plotter('onefinity.json', safe_z=-120)
 onefinity.feed = 5000
 
-turtle = onefinity.turtle(verbose=True, z=-124)
+onefinity._optimize = True
 
-turtle.goto(725,110)
+turtle = onefinity.turtle(verbose=True, z_draw=-126)
+
+turtle.goto(725,110,-115)
 scale = 0.25
 turtle._isdown = True
-colors = ['Black', 'Blue', 'Light Blue', 'Red', 'Orange', 'Lime']
+colors = ['Black', 'Orange', 'Lime', 'Blue', 'Red', 'Light Blue']
 for x in range(360):
+    turtle._z_draw = onefinity.dict['Plotter']['Z-Touch']
     turtle.pencolor(colors[x%6])
     turtle.forward(x*scale)
     turtle.left(59.8)
 
 onefinity.pen_color = None
-onefinity.print_gcode()
+
+onefinity.go()
